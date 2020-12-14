e89a853c94091365adb03d5d17978ac735c09d7f
auleki <kodagiwa@gmail.com>
2020-12-14 08:44:09 +0100

    exercise completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,35 @@
 const bg = document.querySelector('.pepsi')
+const bgColor = document.querySelector('.sec')
 const blackPepsi = document.querySelector('.black')
-const redPepsi = document.querySelector('.red')
+const bluePepsi = document.querySelector('.red')
 const whitePepsi = document.querySelector('.white')
 
+const colorList = {
+  blue: '#0062be',
+  red: '#e60c26',
+  black: '#1e1e1e'
+}
 
 const imageSlider = (anything) => {
   bg.src = anything
 }
 
-blackPepsi.addEventListener('click', () => imageSlider('assets/images/pepsi003.png'))
-whitePepsi.addEventListener('click', () => imageSlider('assets/images/pepsi002.png'))
-redPepsi.addEventListener('click', () => imageSlider('assets/images/pepsi001.png'))
+const changeColor = (color) => {
+  bgColor.style.background = color
+}
+
+blackPepsi.addEventListener('click', () => {
+  imageSlider('assets/images/pepsi003.png')
+  changeColor(colorList.black)
+})
+whitePepsi.addEventListener('click', () => {
+  imageSlider('assets/images/pepsi002.png')
+  changeColor(colorList.red)
+})
+bluePepsi.addEventListener('click', () => {
+  imageSlider('assets/images/pepsi001.png')
+  changeColor(colorList.blue)
+})
 
 // use a switch statment to pass in the values
 
